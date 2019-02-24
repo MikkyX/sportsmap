bbe1f4917c33726f909cdca23700f0bb1025819b
Michael Price <mikkyx@gmail.com>
2019-02-24 14:51:29 +0000

    switch lat and long in venue factory

--- database/factories/VenueFactory.php
+++ database/factories/VenueFactory.php
@@ -16,8 +16,8 @@ $factory->define(
             'name' => $faker->company(),
             'address' => $faker->streetAddress(),
             'postcode' => $faker->postcode(),
-            'latitude' => $faker->latitude(-2.7246891434, -0.5325733597),
-            'longitude' => $faker->longitude(53.4054960449, 54.9674312464),
+            'latitude' => $faker->latitude(53.4054960449, 54.9674312464),
+            'longitude' => $faker->longitude(-2.7246891434, -0.5325733597),
             'website' => 'http://'.$faker->domainName(),
             'telephone' => $faker->phoneNumber(),
         ];
